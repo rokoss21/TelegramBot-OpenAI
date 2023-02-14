fbe18e63753249aabfa48577ef16bed11e126f77
rokoss21 <87014618+rokoss21@users.noreply.github.com>
2023-02-15 02:17:56 +0300

    Version 4.0
    - added markup of response messages and indents

--- ChatGPT_TeleBot.py
+++ ChatGPT_TeleBot.py
@@ -1,6 +1,8 @@
 import openai
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
 
 # Set up OpenAI API key
 openai.api_key = "OPENAI TOKEN"
@@ -9,27 +11,32 @@ openai.api_key = "OPENAI TOKEN"
 bot_token = "TELEGRAM BOT TOKEN"
 bot = telegram.Bot(token=bot_token)
 
+
 # Define a command handler to respond to the /start command
-def start(update, context):
+def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hi! I'm a bot that can help you with anything. What can I do for you?")
 
+
 # Define a message handler to respond to user messages
-def message_handler(update, context):
+def message_handler(update: Update, context: CallbackContext):
     # Get the user's message
     message = update.message.text
 
     # Use OpenAI to generate a response to the user's message
     response = openai.Completion.create(
-        engine="davinci",
+        engine="text-davinci-003",
         prompt=message,
-        max_tokens=60
+        max_tokens=2048
     )
 
     # Send the response back to the user
-    update.message.reply_text(response.choices[0].text)
+    bot.send_message(chat_id=update.message.chat_id, text=response.choices[0].text)
+
 
 # Set up the Telegram bot with the command and message handlers
-updater = Updater(bot_token, use_context=True)
+updater = Updater(bot_token)
+
+# Add handlers for the /start command and user messages
 updater.dispatcher.add_handler(CommandHandler("start", start))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 
